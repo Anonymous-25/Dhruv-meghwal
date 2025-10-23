d23fcc44866ed3b57e205a025065450f3906fa07
Anonymous-25 <dhruvmeghwal252009@gmail.com>
2025-10-23 15:24:10 +0530

    #

--- App/MySite/settings.py
+++ App/MySite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,8 +24,8 @@ SECRET_KEY = "django-insecure-d95*x@&$6ibn2nn71dfvsc^vi#+4(-@4uho=g^exo6fxfroz6h
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['dhruv-meghwal-byin.onrender.com']
 
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
